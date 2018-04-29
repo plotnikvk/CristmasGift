b6199bde1ead7cf04eb78e197434c84ea5d1eac2
plotnikvk <plotnikov.vova@mail.ru>
2018-04-29 09:37:10 +0300

    Update 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-     Gift[]  gifts = new Gift[5];
-     Candy candy = new Candy("Барбарис", 15, 5, "Прилипает к зубам");
-     Candy candy1 = new Candy("Леденец", 25, 7, "Круглый и на палочке");
-     Biscuit cake = new Biscuit("Медовик", 500, 150, "Очень сладкий");
-     Marshmallow marshmallow = new Marshmallow("Зефир", 50, 30,"Тает во рту" );
-     JellyBean jellyBean = new JellyBean("Мармелад", 30, 40, "Тянется как резина" );
+     Gift[]  gifts = new Gift[5];// Создаем массив объектов типа Подарок
 
+        /**
+         * Создаем объекты входящих в подарок сладостей. Передаем параметры для инизиализации
+         */
+        Candy candy = new Candy("Барбарис", 15, 5, "Прилипает к зубам");
+        Candy candy1 = new Candy("Леденец", 25, 7, "Круглый и на палочке");
+        Biscuit cake = new Biscuit("Медовик", 500, 150, "Очень сладкий");
+        Marshmallow marshmallow = new Marshmallow("Зефир", 50, 30,"Тает во рту" );
+        JellyBean jellyBean = new JellyBean("Мармелад", 30, 40, "Тянется как резина" );
+
+        /**
+         * Присваиваем ячейкам массива Подарков объекты наших сладостей
+         */
      gifts[0] = candy;
      gifts[1] = candy1;
      gifts[2] = cake;
@@ -18,7 +25,9 @@ public class Main {
 
      int totalWeight = 0;
      int totalPrice = 0;
-
+/*
+Подсчитываем общий вес, общую стоимость и выводим информацию о подарке
+ */
      System.out.println("Подарок содержит:");
 
      for (int i = 0; i <gifts.length; i++){
